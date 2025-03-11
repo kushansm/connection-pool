231635397d3ffd740cbd997bc3754890a1f247d4
kushansm <kushandileep12@mail.com>
2025-03-11 18:32:27 +0530

    Add mysql connector

--- src/main/java/lk/ijse/dep13/interthreadcommunication/ObtainConnectionServlet.java
+++ src/main/java/lk/ijse/dep13/interthreadcommunication/ObtainConnectionServlet.java
@@ -5,14 +5,22 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.dep13.interthreadcommunication.db.MontisoriCP;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "obtain-connection-servlet", urlPatterns = "/connections/random")
 public class ObtainConnectionServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Obtain connection servlet");
+       MontisoriCP datasource = (MontisoriCP) getServletContext().getAttribute("datasource");
+       MontisoriCP.ConnectionWrapper cWrapper = datasource.getConnection();
+
+       resp.setContentType("text/html");
+       PrintWriter out =resp.getWriter();
+       out.printf("<h1>Id: %s</h1>", cWrapper.id());
+       out.printf("<h1>Connection: %s</h1>", cWrapper.connection());
     }
 }
